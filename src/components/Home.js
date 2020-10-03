b5e64a259b773c39f3b62f28a5a8f8aa205fa305
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-10-02 18:12:28 -0600

    added styled components

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+    return (
+        <MainDiv>
+            Hello World
+        </MainDiv>
+    )
+}
+
+const MainDiv = styled.div`
+    font-size: 24px;
+`
+
+export default Home;
